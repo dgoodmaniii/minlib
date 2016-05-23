cf95ee595485384b2e229e96052ac505f037b751
Donald P. Goodman III <dgoodmaniii@gmail.com>
2016-05-23 12:24:08 -0400

    Adding metadata with -a now works.
    For files *and* directories.

--- add_files.c
+++ add_files.c
@@ -7,10 +7,14 @@
  * Son, and the Holy Spirit, for their mercy.
  */
 
+#define _XOPEN_SOURCE 500
 #include<stdio.h>
 #include<stdlib.h>
 #include<string.h>
 #include<errno.h>
+#include<ftw.h>
+#include<unistd.h>
+#include<limits.h>
 #include<sys/stat.h>
 #include"errcodes.h"
 
@@ -37,14 +41,102 @@ int add_file(char *s)
 {
 	if (strstr(s,".pdf"))
 		pdf_metadata(s);
+	if (strstr(s,".epub"))
+		epub_metadata(s);
 	return 0;
 }
 
-int get_pdf_meta(char *s);
+int dummy(const char *fpath, const struct stat *sb,
+	int tflag, struct FTW *ftwbuf)
+{
+	char *s;
+
+	if ((s = malloc((strlen(fpath) + 1) * sizeof(char))) == NULL) {
+		fprintf(stderr,"minlib:  insufficient memory to store "
+		"the next point of the directory tree\n");
+		exit(INSUFF_INTERNAL_MEMORY);
+	}
+	strcpy(s,fpath);
+	if (S_ISREG(sb->st_mode))
+		add_file(s);
+	free(s);
+	return 0;
+}
 
 /* the following calls add_file for each file it sees */
 int add_dir(char *s)
 {
+	if (nftw(s,dummy,20,0) != 0);
+	return 0;
+}
+
+
+int epub_metadata(char *s)
+{
+	int i, j, c;
+	char *line = NULL;
+	ssize_t read; size_t len = 0;
+	FILE *p;
+	char *ptr;
+	char *t;
+	char fhead[MAX_FIELD_LEN+1];
+	char text[MAX_TITLE_LEN+1];
+	char actpath[MAX_TITLE_LEN+1];
+	int ind, oind;
+
+	if ((t = malloc((strlen(s) + 26) * sizeof(char))) == NULL) {
+		fprintf(stderr,"minlib:  insufficient memory to store "
+		"command necessary to extra epub metadata\n");
+		exit(INSUFF_INTERNAL_MEMORY);
+	}
+	*t = '\0';
+	printf("%%%%\n");
+	strcat(t,"unzip -c ");
+	strcat(t,s);
+	strcat(t," *content.opf");
+	if ((p = popen(t,"r")) == NULL) {
+		fprintf(stderr,"minlib:  error opening file \"%s\" "
+		"with error code %d:\n\t%s\n",s,errno,strerror(errno));
+		exit(ERROR_OPEN_ADDFILE);
+	}
+	while ((read = getline(&line,&len,p)) != -1) {
+		if (ptr = strstr(line,"dc:")) {
+			ind = 0;
+			for (i = 3; ptr[i] != '\0' && isalpha(ptr[i]); ++i) {
+				fhead[ind++] = ptr[i];
+			}
+			fhead[ind] = '\0';
+			if (!strcmp(fhead,"contributor")) {
+				if (strstr(line,"edt"))
+					strcpy(fhead,"EDITOR");
+			}
+			if (!strcmp(fhead,"contributor")) {
+				if (strstr(line,"trl"))
+					strcpy(fhead,"TRANS");
+			}
+			if (!strcmp(fhead,"creator"))
+				strcpy(fhead,"AUTHOR");
+			if (!strcmp(fhead,"AUTHOR") || !strcmp(fhead,"title")
+			|| !strcmp(fhead,"language") || !strcmp(fhead,"subject")
+			|| !strcmp(fhead,"rights") || !strcmp(fhead,"contributor")
+			|| !strcmp(fhead,"EDITOR") ||  !strcmp(fhead,"source")
+			|| !strcmp(fhead,"TRANS")) {
+				while(line[i++] != '>');
+				oind = 0;
+				while((line[i] != '<') && (line[i] != '\0'))
+					text[oind++] = line[i++];
+				text[oind] = '\0';
+				for (j = 0; fhead[j] != '\0'; ++j)
+					printf("%c",toupper(fhead[j]));
+				printf(":\t%s\n",text);
+				fhead[0] = '\0'; text[0] = '\0';
+			}
+		}
+	}
+	printf("PATH:\t%s\n",realpath(s,actpath));
+	free(t);
+	free(line);
+	pclose(p);
 	return 0;
 }
 
@@ -65,21 +157,16 @@ int pdf_metadata(char *s)
 		"with error code %d:\n\t%s\n",s,errno,strerror(errno));
 		exit(ERROR_OPEN_ADDFILE);
 	}
-	printf("%%%%\n");
 	if ((fstat(fileno(fp),&st) != 0)) {
 		fprintf(stderr,"minlib:  cannot determine size of file "
 		"%s\n",s);
 		exit(ERROR_SIZE_ADDFILE);
 	}
 	file_size = st.st_size;
+	printf("%%%%\n");
 	while (curr_byte < file_size) {
 		++curr_byte;
 		c = fgetc(fp);
-		if ((c == '%') && ((c = fgetc(fp)) == '%') && ((c =
-		fgetc(fp)) == 'E') && ((c = fgetc(fp)) == 'O') && ((c
-		= fgetc(fp)) == 'F') && ((c = fgetc(fp)) != '\n')) {
-			break;
-		}
 		if (c == '/') {
 			while(isalpha(c = fgetc(fp)) && (ind < MAX_FIELD_LEN))
 				fhead[ind++] = c;
@@ -109,6 +196,6 @@ int pdf_metadata(char *s)
 		}
 	}
 	printf("PATH:\t%s\n",realpath(s,actpath));
-	close(fp);
+	fclose(fp);
 	return 0;
 }
--- test.sh
+++ test.sh
@@ -2,5 +2,7 @@
 # +AMDG
 
 make;
-valgrind -v --leak-check=full --track-origins=yes ./minlib -a "greekfathers00fortgoog.pdf"
+#valgrind -v --leak-check=full --track-origins=yes ./minlib -a "greekfathers00fortgoog.pdf"
+#valgrind -v --leak-check=full --track-origins=yes ./minlib -a "cockerille_bookding.epub" 2> errors
+valgrind -v --leak-check=full --track-origins=yes ./minlib -a "../" 2> errors
 #valgrind -v --leak-check=full --track-origins=yes ./minlib
