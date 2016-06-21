7f27cb156639380e8a49b8428de7939bf3b701e5
Donald P. Goodman III <dgoodmaniii@gmail.com>
2016-06-21 11:35:23 -0400

    Got the unique tag stats working.

--- stats.c
+++ stats.c
@@ -17,6 +17,11 @@ struct tags {
 	int number;
 };
 
+struct vals {
+	char tag[24];
+	char *value;
+};
+
 int stats_report(int *recnums, char **ptr, char **formlist,
 					int numrecs, int statsval)
 {
@@ -31,15 +36,19 @@ int stats_report(int *recnums, char **ptr, char **formlist,
 
 int print_stats(char **ptr, int numrecs)
 {
-	int i; int j;
+	int i; int j; int k = 0;
 	struct tags *datalines;
+	struct tags *uniqlines;
+	struct vals *datavals;
 	char tag[24] = "";
 	int numtags = 0;
 	int tagind = 0;
+	int valind = 0;
 
 	datalines = malloc(numtags * sizeof(struct tags));
+	uniqlines = malloc(numtags * sizeof(struct tags));
+	datavals = malloc(numtags * sizeof(struct vals));
 	printf("TOTAL RECORDS:\t\t%4d\n",numrecs);
-	printf("TAG STATISTICS:\n");
 	for (i = 0, j = 0; *(ptr+i) != NULL; ++i, ++j) {
 		if (strstr(*(ptr+i),"%%"))
 			--j;
@@ -49,28 +58,63 @@ int print_stats(char **ptr, int numrecs)
 				++numtags;
 				datalines = realloc(datalines,numtags*sizeof(struct tags));
 				strcpy(datalines[numtags-1].tag,tag);
+				uniqlines = realloc(uniqlines,numtags*sizeof(struct tags));
+				strcpy(uniqlines[numtags-1].tag,tag);
 				datalines[numtags-1].number = 1;
-			} else {
-				++datalines[tagind].number;
+				uniqlines[numtags-1].number = 1;
+				datavals = realloc(datavals,(k+1)*sizeof(struct vals));
+				strcpy(datavals[k].tag,tag);
+				datavals[k].value = *(ptr+i+1);
+				++k;
+			} else { /* FIXME */
+				++(datalines[tagind].number);
+				if ((valind = in_full_db(tag,*(ptr+i+1),datavals,k)) == -1) {
+					++(uniqlines[tagind].number);
+					datavals = realloc(datavals,(k+1) * sizeof(struct vals));
+					strcpy(datavals[k].tag,tag);
+					datavals[k].value = *(ptr+i+1);
+					++k;
+				}
 			}
 		}
 	}
+	printf("TAG STATISTICS:\n");
 	for (i = 0; i < numtags; ++i) {
 		printf("\t%s:",datalines[i].tag);
 		(strlen(datalines[i].tag) < 7) ? printf("\t\t") : printf("\t");
 		printf("%4d\n",datalines[i].number);
 	}
 	printf("UNIQUE VALUES:\n");
+	for (i = 0; i < numtags; ++i) {
+		printf("\t%s:",uniqlines[i].tag);
+		(strlen(uniqlines[i].tag) < 7) ? printf("\t\t") : printf("\t");
+		printf("%4d\n",uniqlines[i].number);
+	}
+	free(datalines);
+	free(uniqlines);
+	free(datavals);
 	return 0;
 }
 
 /* returns number of tag's index if found, -1 if not  */
-int in_db(char *s, struct tags *datalines,int numtags)
+int in_db(char *s, struct tags *datalines, int numtags)
 {
 	int i;
 
-	for (i = 0; i <= numtags; ++i) {
+	for (i = 0; i < numtags; ++i) {
 		if (!strcmp(datalines[i].tag,s)) {
+			return i;
+		}
+	}
+	return -1;
+}
+
+int in_full_db(char *s, char *t, struct vals *datavals, int totalrecs)
+{
+	int i;
+
+	for (i = 0; i < totalrecs; ++i) {
+		if (!strcmp(datavals[i].tag,s) && !strcmp(datavals[i].value,t)) {
 			return i;
 		}
 	}
